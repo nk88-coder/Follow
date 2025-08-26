5ed275d264eaf86bb18a82c82f2bb29e046a35cc
nk88-coder <sdnishokkhumar@gmail.com>
2025-08-26 09:20:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+  const img = document.getElementById('myPhoto');
+  img.addEventListener('click', () => {
+    img.classList.toggle('zoomed');
+  });
+});
